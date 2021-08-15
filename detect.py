9ee0420eb025bb7314d69fe3921a31b9be0c8697
chLi <565353780@qq.com>
2021-08-15 15:07:20 +0800

    add source code

---
+++ detect.py
@@ -0,0 +1,132 @@
+import sys
+import time
+from pathlib import Path
+
+import cv2
+import torch
+#  import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).absolute()
+sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
+
+from models.experimental import attempt_load
+from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
+    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
+from utils.plots import colors, plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_sync
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+@torch.no_grad()
+def detect():
+    weights='yolov5l6.pt' # model.pt path(s)
+    source='../jittor-yolo/sample_images'
+    imgsz=640 # inference size (pixels)
+
+    # Initialize
+    device = select_device('')
+    half = True
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    w = weights[0] if isinstance(weights, list) else weights
+    pt = w.endswith('.pt') # inference type
+    stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    if half:
+        model.half()  # to FP16
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    bs = 1  # batch_size
+
+    # Run inference
+    if pt and device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
+
+        # Inference
+        t1 = time_sync()
+        #  visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if False else False
+        pred = model(img, augment=False, visualize=False)[0]
+
+        # NMS
+        pred = non_max_suppression(pred, max_det=1000)
+        t2 = time_sync()
+
+        # Process predictions
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0  # for save_crop
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                #  Write results
+                #  for *xyxy, conf, cls in reversed(det):
+
+                    #  if save_img or save_crop or view_img:  # Add bbox to image
+                        #  c = int(cls)  # integer class
+                        #  label = None if False else (names[c] if False else f'{names[c]} {conf:.2f}')
+                        #  plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
+                        #  if save_crop:
+                            #  save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+            #  cv2.imshow(str(p), im0)
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == "__main__":
+    #  check_requirements(exclude=('tensorboard', 'thop'))
+    detect()
