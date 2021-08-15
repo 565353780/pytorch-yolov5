6a958972b9df7c7b13fa1926553b1da4e989f221
chLi <565353780@qq.com>
2021-08-15 15:49:13 +0800

    fix bug, change letterbox to correct version

--- detect.py
+++ detect.py
@@ -1,7 +1,9 @@
+import os
 import sys
 import time
 from pathlib import Path
 
+import numpy as np
 import cv2
 import torch
 #  import torch.backends.cudnn as cudnn
@@ -15,15 +17,15 @@ from utils.general import check_img_size, check_requirements, check_imshow, colo
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_sync
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
-    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
-    shape = img.shape[:2]  # current shape [height, width]
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
 
     # Scale ratio (new / old)
     r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
         r = min(r, 1.0)
 
     # Compute padding
@@ -31,7 +33,7 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
     dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
     if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
     elif scaleFill:  # stretch
         dw, dh = 0.0, 0.0
         new_unpad = (new_shape[1], new_shape[0])
@@ -41,12 +43,11 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
     dh /= 2
 
     if shape[::-1] != new_unpad:  # resize
-        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
-
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return im, ratio, (dw, dh)
 
 @torch.no_grad()
 def detect():
@@ -75,8 +76,18 @@ def detect():
     # Run inference
     if pt and device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
+
+    image_folder_path = "./sample_images"
+
+    image_file_name_list = os.listdir(image_folder_path)
+
+    for image_file_name in image_file_name_list:
+        img_source = cv2.imread(os.path.join(image_folder_path, image_file_name))
+
+        img = letterbox(img_source, new_shape=(640, 640), stride=stride)[0]
+        img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        img = np.ascontiguousarray(img)
+
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -84,22 +95,19 @@ def detect():
             img = img[None]  # expand for batch dim
 
         # Inference
-        t1 = time_sync()
         #  visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if False else False
         pred = model(img, augment=False, visualize=False)[0]
 
         # NMS
         pred = non_max_suppression(pred, max_det=1000)
-        t2 = time_sync()
 
         # Process predictions
         for i, det in enumerate(pred):  # detections per image
-            p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+            im0 = img_source.copy()
 
-            p = Path(p)  # to Path
-            s += '%gx%g ' % img.shape[2:]  # print string
+            detect_result = ""
+
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0  # for save_crop
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -107,25 +115,18 @@ def detect():
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    detect_result += str(int(n)) + " " + names[int(c.int())] + ", "
 
-                #  Write results
-                #  for *xyxy, conf, cls in reversed(det):
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
 
-                    #  if save_img or save_crop or view_img:  # Add bbox to image
-                        #  c = int(cls)  # integer class
-                        #  label = None if False else (names[c] if False else f'{names[c]} {conf:.2f}')
-                        #  plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
-                        #  if save_crop:
-                            #  save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                    label = None if False else (names[c] if False else f'{names[c]} {conf:.2f}')
+                    plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
-
-            #  cv2.imshow(str(p), im0)
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
-
+            #  cv2.imshow("test", im0)
+            #  cv2.waitKey(0)
+            print(detect_result)
 
 if __name__ == "__main__":
     #  check_requirements(exclude=('tensorboard', 'thop'))
