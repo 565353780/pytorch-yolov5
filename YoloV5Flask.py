480ea743415b239c4054a821a873190c0015b243
chLi <565353780@qq.com>
2021-10-29 13:02:12 +0800

    fix all bug, need test

--- YoloV5Flask.py
+++ YoloV5Flask.py
@@ -12,6 +12,17 @@ import base64
 
 app = Flask(__name__)
 
+def getBytesFromSourceBytesStr(bytes_str):
+    bytes_str_copy = bytes_str
+    if "," in bytes_str_copy:
+        bytes_str_cpoy = bytes_str_copy.split(",")[1]
+
+    if bytes_str_copy[0] == 'b':
+        bytes_str_copy = bytes_str_copy[2:-1]
+
+    bytes_return = bytes(bytes_str_copy, encoding="utf-8")
+    return bytes_return
+
 pytorch_yolov5_detector = PyTorchYoloV5Detector()
 pytorch_yolov5_detector.loadModel('./yolov5s.pt', 'cuda:0')
 
@@ -21,11 +32,11 @@ def detect_http():
         data = request.get_data()
         data_json = json.loads(data)
 
-        if "," in data_json:
-            data_json = data_json.split(",")[1]
+        bytes_str = data_json["img"]
 
-        img_b64encode = bytes(data_json["img"][2:-1], encoding="utf-8")
-        img_b64decode = base64.b64decode(img_b64encode)
+        bytes_return = getBytesFromSourceBytesStr(bytes_str)
+
+        img_b64decode = base64.b64decode(bytes_return)
 
         img_array = np.frombuffer(img_b64decode,np.uint8)
         img = cv2.imdecode(img_array,cv2.COLOR_BGR2RGB)
--- test_flask.py
+++ test_flask.py
@@ -17,3 +17,20 @@ if image is None:
     print("base64 -> Image = None!")
 else:
     print(image.shape)
+
+# socket_data -> opencv
+socket_data = None
+with open("/home/chli/chLi/test1.txt", "r") as f:
+    socket_data = f.readlines()[0]
+if "," in socket_data:
+    socket_data = socket_data.split(",")[1]
+if socket_data[0] == 'b':
+    socket_data = socket_data[2:-1]
+
+img_b64encode = bytes(socket_data, encoding="utf-8")
+img_b64decode = base64.b64decode(img_b64encode)
+
+img_array = np.frombuffer(img_b64decode,np.uint8)
+img = cv2.imdecode(img_array,cv2.COLOR_BGR2RGB)
+print(img.shape)
+cv2.imwrite("/home/chli/chLi/read_socket.jpg", img)
