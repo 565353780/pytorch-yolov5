c9ccde9b86b538f88bf83c2df96647c14fca2f1d
chLi <565353780@qq.com>
2021-12-28 21:26:21 +0800

    add image resize

--- VisualDetect.py
+++ VisualDetect.py
@@ -11,21 +11,24 @@ pytorch_yolov5_detector.loadModel('./yolov5s.pt', 'cuda:0')
 jetson_camera = JetsonCamera()
 jetson_camera.loadCapture()
 
+window_name = "Visual Detect"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
-    print("start get image")
     image = jetson_camera.grabImage()
-    print("finish get image")
-    print("start detect")
     if image is None:
         break
+    scale = 0.5
+    image = image.resize(image, (image.shape[1] * scale, image.shape[0] * scale))
+    print("image shape is ", image.shape)
     result = pytorch_yolov5_detector.detect(image)
-    print("finish detect")
     for single_object in result:
         x_min, y_min, x_max, y_max = result[0]
         label = result[1]
         label_str = result[2]
         prob = result[3]
         cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
-    cv2.imshow("Visual Detect", image)
+    cv2.imshow(window_name, image)
     cv2.waitKey(1)
 
