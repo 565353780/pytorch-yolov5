3371f994e52eeb122290269394a85b59437e3577
chLi <565353780@qq.com>
2021-12-28 23:19:12 +0800

    add playsound usage

--- README.md
+++ README.md
@@ -4,17 +4,26 @@
 
 ```bash
 sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
+```
 
+### for x86
+```bash
 conda create -n torch python=3.8
 conda activate torch
-pip install matplotlib==3.2.2 # for arm only
-pip install -U scikit-build flask pandas opencv-python pyyaml seaborn requests tqdm tensorboard future setuptools Cython
-
-# for x86
 pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-# for arm only
+pip install -U scikit-build flask pandas opencv-python pyyaml seaborn requests tqdm tensorboard future setuptools Cython playsound
+```
+
+### for Jetson
+Prepare Environment first!
+
+https://blog.csdn.net/qq_49466306/article/details/108867092 
+
+```bash
+pip3 install matplotlib==3.2.2
+pip3 install -U scikit-build flask pandas pyyaml seaborn requests tqdm tensorboard future setuptools Cython playsound
 wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl
-pip install torch-<...>.whl
+pip3 install torch-<...>.whl
 cd ..
 git clone --branch release/0.11 https://github.com/pytorch/vision torchvision
 cd torchvision
@@ -27,6 +36,13 @@ cd ../pytorch-yolov5
 
 ```bash
 python PyTorchYoloV5Detector.py
+```
+
+## Run for Jetson
+
+```bash
+python3 VisualDetect.py
+python3 DetectSaver.py
 ```
 
 ### If Memory Error occured
--- VisualDetect.py
+++ VisualDetect.py
@@ -4,8 +4,36 @@
 import os
 import cv2
 from JetsonCamera import JetsonCamera
+from playsound import playsound
+
+'''
+User Edit Area Start
+'''
 
 scale = 0.5
+
+def post_process(image, result):
+    for single_object in result:
+        single_object_split = single_object.split("_")
+        if len(single_object_split) < 6:
+            continue
+        x_min = int(single_object_split[0])
+        y_min = int(single_object_split[1])
+        x_max = int(single_object_split[2])
+        y_max = int(single_object_split[3])
+        label = int(single_object_split[4])
+        label_str = single_object_split[5]
+        score = float(single_object_split[6])
+        cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+
+    #  if "Seat belt" not in result:
+        #  playsound("./tip.mp3")
+
+    return True
+
+'''
+User Edit Area End
+'''
 
 jetson_camera = JetsonCamera()
 jetson_camera.loadCapture()
@@ -46,18 +74,8 @@ while jetson_camera.captureImage():
 
     with open("./trans_camera_result.txt", "r") as f:
         result = f.readlines()
-        for single_object in result:
-            single_object_split = single_object.split("_")
-            if len(single_object_split) < 6:
-                continue
-            x_min = int(single_object_split[0])
-            y_min = int(single_object_split[1])
-            x_max = int(single_object_split[2])
-            y_max = int(single_object_split[3])
-            label = int(single_object_split[4])
-            label_str = single_object_split[5]
-            score = float(single_object_split[6])
-            cv2.rectangle(detecting_image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+        post_process(detecting_image, result)
+
     os.remove("./trans_camera_result.txt")
     os.remove("./trans_camera_result_ok.txt")
     cv2.imwrite("./trans_camera.jpg", realtime_iamge)
