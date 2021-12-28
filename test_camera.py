89317ef51533e44a51313ca924714e63c12e9fc1
chLi <565353780@qq.com>
2021-12-28 20:33:39 +0800

    fix camera bug by using default python3

--- test_camera.py
+++ test_camera.py
@@ -3,30 +3,55 @@
 
 import cv2
 
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-capture.set(cv2.CAP_PROP_FPS, 60)
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-window_name = "Camera"
 
-window_handle = cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imshow("CSI Camera", img)
+            # This also acts as
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
 
-while cv2.getWindowProperty(window_name, 0) >= 0:
-    ret, frame = capture.read()
 
-    if frame is None:
-        print("frame is None!")
-        break
-
-    print(frame.shape)
-    cv2.imshow(window_name, frame)
-
-    keyCode = cv2.waitKey(1) & 0xFF
-    if keyCode == 27:# ESC
-        break
-
-capture.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    show_camera()
 
