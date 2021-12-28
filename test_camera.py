a9b3ffe150ac06710e89d83a4bc0b9bfd1370865
chLi <565353780@qq.com>
2021-12-28 19:31:32 +0800

    simplify cv2 videocap code

--- test_camera.py
+++ test_camera.py
@@ -3,27 +3,18 @@
 
 import cv2
 
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 window_name = "Camera"
 
-window_handle = cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+while True:
+    ret, frame = capture.read()
 
-while cv2.getWindowProperty(window_name, 0) >= 0:
-    ret_val, img = cap.read()
+    cv2.imshow(window_name, frame)
 
-    height, width = img.shape[0:2]
-    if width>800:
-        new_width=800
-        new_height=int(height * new_width / width)
-        img = cv2.resize(img, (new_width,new_height))
-
-    cv2.imshow(window_name, img)
-
-    keyCode = cv2.waitKey(30) & 0xFF
+    keyCode = cv2.waitKey(1) & 0xFF
     if keyCode == 27:# ESC
         break
 
-cap.release()
-cv2.destroyAllWindows()
+capture.release()
 
