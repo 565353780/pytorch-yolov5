1a73c65c3e2dfb5d60b43c1be244c7734db7aa68
chLi <565353780@qq.com>
2021-12-28 19:41:52 +0800

    use gstream for camera

--- test_camera.py
+++ test_camera.py
@@ -3,26 +3,58 @@
 
 import cv2
 
-capture = cv2.VideoCapture(0)
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-window_name = "Camera"
+if __name__ == "__main__":
+    capture_width = 1280
+    capture_height = 720
+    display_width = 1280
+    display_height = 720
+    framerate = 60
+    flip_method = 0
 
-window_handle = cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+    print(gstreamer_pipeline(capture_width,capture_height,display_width,display_height,framerate,flip_method))
 
-while cv2.getWindowProperty(window_name, 0) >= 0:
-    ret, frame = capture.read()
+    cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 
-    if frame is None:
-        print("frame is None!")
-        break
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
 
-    print(frame.shape)
-    cv2.imshow(window_name, frame)
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imshow("CSI Camera", img)
 
-    keyCode = cv2.waitKey(1) & 0xFF
-    if keyCode == 27:# ESC
-        break
+            keyCode = cv2.waitKey(30) & 0xFF
+            if keyCode == 27:# ESC
+                break
 
-capture.release()
-cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Open Camera Failed!")
 
